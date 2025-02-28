c2234ef3636350d1a72fb232d01f2a9ab9af6157
yagnikEancton <yvpanchal@enacton.com>
2025-02-28 11:32:23 +0530

    a commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native', // your existing base configuration
+    'plugin:jest/recommended', // Add Jest plugin and recommended rules
+  ],
+  plugins: ['jest'],
+  env: {
+    jest: true, // Add Jest globals for testing files
+  },
+  rules: {
+    'jest/expect-expect': 'off', // Disable the expect-expect rule
+  },
 };
